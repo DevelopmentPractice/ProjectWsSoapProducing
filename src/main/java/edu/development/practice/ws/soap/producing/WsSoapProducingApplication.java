f4d53cd21a1f5b62d7c0f973e3926f6e8a6ddc53
Angel Martinez <angelocc985@gmail.com>
2023-07-20 21:15:27 -0600

    Se sube proyecto inicial

---
+++ src/main/java/edu/development/practice/ws/soap/producing/WsSoapProducingApplication.java
@@ -0,0 +1,13 @@
+package edu.development.practice.ws.soap.producing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsSoapProducingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsSoapProducingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/development/practice/ws/soap/producing/WsSoapProducingApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.development.practice.ws.soap.producing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsSoapProducingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
